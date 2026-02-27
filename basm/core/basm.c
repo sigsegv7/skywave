f4657daca4e31e1ff73fe158642e4b6d980b70ed
Ian Moffett <ian@mirocom.org>
2026-02-27 17:05:45 -0500

    basm: Parse command line options
    Signed-off-by: Ian Moffett <ian@mirocom.org>

--- basm/core/basm.c
+++ basm/core/basm.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /* BASM version */
 #define BASM_VERSION "0.0.1"
 
+static void
+help(void)
+{
+    printf(
+        "BASM assembler - skywave assembler\n"
+        "----------------------------------\n"
+        "[-h] Display help menu\n"
+        "[-v] Display version\n"
+    );
+}
+
+static void
+version(void)
+{
+    printf(
+        "BASM assembler - skywave assembler\n"
+        "----------------------------------\n"
+        "Copyright (c) 2026, Ian Moffett\n"
+        "Version v%s\n",
+        BASM_VERSION
+    );
+}
+
 int
 main(int argc, char **argv)
 {
-    printf("basm %s", BASM_VERSION);
+    int opt;
+
+    while ((opt = getopt(argc, argv, "hv")) != -1) {
+        switch (opt) {
+        case 'h':
+            help();
+            return -1;
+        case 'v':
+            version();
+            return -1;
+        }
+    }
+
     return 0;
 }
