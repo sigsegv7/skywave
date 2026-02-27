6208acd6f56ec1ae4f238df79d2b419d34cd047e
Ian Moffett <ian@mirocom.org>
2026-02-26 21:56:51 -0500

    rtl: Rethink fetch unit
    Signed-off-by: Ian Moffett <ian@mirocom.org>

--- rtl/cpu/ctl.sv
+++ rtl/cpu/ctl.sv
@@ -5,43 +5,21 @@
 //
 // @clk_i:          Clock input
 // @reset_i:        Reset input
-// @inst_i:         Instruction input
-// @inst_ready_i:   Instruction ready input
-// @inst_consume_o: Instruction consume output
 //
 module ctl (
     input wire clk_i,
-    input wire reset_i,
-    input wire [31:0] inst_i,
-    input wire inst_ready_i,
-
-    output logic inst_consume_o
+    input wire reset_i
 );
-    logic pc_inhibit;
-    logic substage;
     /* verilator lint_off UNUSEDSIGNAL */
-    logic [31:0] inst;
+    logic pc_inhibit;
+    logic [2:0] substage;
+    logic inst_ready;
 
     always_ff @(posedge clk_i) begin
         if (reset_i) begin
             pc_inhibit <= 0;
-            inst_consume_o <= 0;
             substage <= 0;
-        end else if (!pc_inhibit && !inst_ready_i) begin
-            inst_consume_o <= 1;
-        end else if (!pc_inhibit && inst_ready_i && !substage) begin
-            substage <= ~substage;
-            inst <= inst_i;
-        end else if (!pc_inhibit && inst_ready_i && substage) begin
-            inst_consume_o <= 0;
-            substage <= 0;
-            case (inst[7:0])
-                OPCODE_NOP:  ;
-                OPCODE_HLT: pc_inhibit <= 1;
-                default: ;
-            endcase
-        end else if (pc_inhibit) begin
-            inst_consume_o <= 0;
+            inst_ready <= 0;
         end
     end
 endmodule
--- rtl/cpu/pe.sv
+++ rtl/cpu/pe.sv
@@ -12,29 +12,18 @@ module pe #(
 ) (
     input wire clk_i,
     input wire reset_i,
+
+    /* verilator lint_off UNUSEDSIGNAL */
     input wire [BUS_WIDTH-1:0] bus_data_i,
 
     output logic [AD_LEN-1:0] bus_ad_o
 );
-    logic [31:0] inst_feed;
-    logic inst_ready_feed;
-    logic inst_consume_feed;
-
-    fetch #(.BUS_WIDTH(BUS_WIDTH), .AD_LEN(AD_LEN)) fetch_unit (
-        .clk_i(clk_i),
-        .reset_i(reset_i),
-        .bus_data_i(bus_data_i),
-        .inst_consume_i(inst_consume_feed),
-        .bus_ad_o(bus_ad_o),
-        .inst_o(inst_feed),
-        .inst_ready_o(inst_ready_feed)
-    );
-
     ctl ctl_unit (
         .clk_i(clk_i),
-        .reset_i(reset_i),
-        .inst_i(inst_feed),
-        .inst_ready_i(inst_ready_feed),
-        .inst_consume_o(inst_consume_feed)
+        .reset_i(reset_i)
     );
+
+    always_ff @(posedge clk_i) begin
+        bus_ad_o <= 0;
+    end
 endmodule
